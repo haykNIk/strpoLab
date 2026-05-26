509c1085d993438eb2aa0780431fc776e33289a8
haykNIk <hayknikiyan@gmail.com>
2026-05-26 11:27:50 +0300

    Update Form.cpp with fill function

--- Form.cpp
+++ Form.cpp
@@ -1,2 +1,10 @@
 #include "Form.h"
-// Пока пустой, позже добавим fill
+#include <cstring>
+
+namespace {
+    void fill(char* str, unsigned int len, char c) {
+        if (len == 0) return;
+        for (unsigned int i = 0; i < len - 1; ++i) str[i] = c;
+        str[len - 1] = '\0';
+    }
+}
