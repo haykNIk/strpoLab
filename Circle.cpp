690b2dd5d0460396a79f214c16b8b28e75fe409c
haykNIk <hayknikiyan@gmail.com>
2026-05-26 11:32:00 +0300

    Update Circle.cpp with fill

--- Circle.cpp
+++ Circle.cpp
@@ -1,17 +1,16 @@
 #include "Circle.h"
 #include <iostream>
+#include "Form.h"
 
 Circle::Circle(unsigned int offset) : Form(offset) {}
 
 void Circle::draw() const {
-    for (unsigned int i = 0; i < m_offset; ++i) std::cout << ' ';
-    std::cout << "  ****  " << std::endl;
-    for (unsigned int i = 0; i < m_offset; ++i) std::cout << ' ';
-    std::cout << " *    * " << std::endl;
-    for (unsigned int i = 0; i < m_offset; ++i) std::cout << ' ';
-    std::cout << "*      *" << std::endl;
-    for (unsigned int i = 0; i < m_offset; ++i) std::cout << ' ';
-    std::cout << " *    * " << std::endl;
-    for (unsigned int i = 0; i < m_offset; ++i) std::cout << ' ';
-    std::cout << "  ****  " << std::endl;
+    const unsigned int MAX = 100;
+    char spaces[MAX];
+    internal::fill(spaces, m_offset + 1, ' ');
+    std::cout << spaces << "  ****  " << std::endl;
+    std::cout << spaces << " *    * " << std::endl;
+    std::cout << spaces << "*      *" << std::endl;
+    std::cout << spaces << " *    * " << std::endl;
+    std::cout << spaces << "  ****  " << std::endl;
 }
